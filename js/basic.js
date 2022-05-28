b80d00faa3b39eea460c4fca8f6ac9a0f9c4ffd2
Yuliia <yliia192@gmail.com>
2022-05-28 18:21:19 +0300

    menu

--- js/basic.js
+++ js/basic.js
@@ -9,6 +9,114 @@ function showImageInModal(imageElement) {
     image.src = URL.createObjectURL(imageElement.files[0]);
 }
 
+//Menu
+function showCatalog() {
+    document.getElementById("menuDropDownCatalog").style.display = "block";
+}
+
+function closeCatalog() {
+    document.getElementById("menuDropDownCatalog").style.display = "none";
+}
+
+function showMenu() {
+    document.getElementById("menuDropDownMenu").style.display = "block";
+}
+
+function closeMenu() {
+    document.getElementById("menuDropDownMenu").style.display = "none";
+}
+
+
+function showImg(key) {
+    switch (key) {
+        case 1:
+            document.getElementById("brother").style.display = "block";
+            document.getElementById("always").style.display = "none";
+            break;
+        case 2:
+            document.getElementById("canon").style.display = "block";
+            document.getElementById("always").style.display = "none";
+            break;
+        case 3:
+            document.getElementById("dell").style.display = "block";
+            document.getElementById("always").style.display = "none";
+            break;
+        case 4:
+            document.getElementById("epson").style.display = "block";
+            document.getElementById("always").style.display = "none";
+            break;
+        case 5:
+            document.getElementById("hp").style.display = "block";
+            document.getElementById("always").style.display = "none";
+            break;
+        case 6:
+            document.getElementById("kodak").style.display = "block";
+            document.getElementById("always").style.display = "none";
+            break;
+        case 7:
+            document.getElementById("kyocera").style.display = "block";
+            document.getElementById("always").style.display = "none";
+            break;
+        case 8:
+            document.getElementById("jetix").style.display = "block";
+            document.getElementById("always").style.display = "none";
+            break;
+        case 9:
+            document.getElementById("polaroid").style.display = "block";
+            document.getElementById("always").style.display = "none";
+            break;
+        case 10:
+            document.getElementById("samsung").style.display = "block";
+            document.getElementById("always").style.display = "none";
+            break;
+    }
+}
+
+function closeImg(key) {
+    switch (key) {
+        case 1:
+            document.getElementById("brother").style.display = "none";
+            document.getElementById("always canon").style.display = "block";
+            break;
+        case 2:
+            document.getElementById("canon").style.display = "none";
+            document.getElementById("always").style.display = "block";
+            break;
+        case 3:
+            document.getElementById("dell").style.display = "none";
+            document.getElementById("always").style.display = "block";
+            break;
+        case 4:
+            document.getElementById("epson").style.display = "none";
+            document.getElementById("always").style.display = "block";
+            break;
+        case 5:
+            document.getElementById("hp").style.display = "none";
+            document.getElementById("always").style.display = "block";
+            break;
+        case 6:
+            document.getElementById("kodak").style.display = "none";
+            document.getElementById("always").style.display = "block";
+            break;
+        case 7:
+            document.getElementById("kyocera").style.display = "none";
+            document.getElementById("always").style.display = "block";
+            break;
+        case 8:
+            document.getElementById("jetix").style.display = "none";
+            document.getElementById("always").style.display = "block";
+            break;
+        case 9:
+            document.getElementById("polaroid").style.display = "none";
+            document.getElementById("always").style.display = "block";
+            break;
+        case 10:
+            document.getElementById("samsung").style.display = "none";
+            document.getElementById("always").style.display = "block";
+            break;
+    }
+}
+
 // Content
 function showContent() {
     document.getElementById("compTable").style.display = "none";
